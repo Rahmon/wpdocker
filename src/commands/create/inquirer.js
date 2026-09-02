896a31c8088df5418ec7e2ef93f2527488f73671
Ramon Ahnert <Rahmon@users.noreply.github.com>
2026-09-01 22:54:33 -0300

    feat(create): offer php 8.4 and 8.5 in the version prompt

--- src/commands/create/inquirer.js
+++ src/commands/create/inquirer.js
@@ -1,7 +1,20 @@
 const { validateNotEmpty, parseHostname, parseProxyUrl } = require('../../prompt-validators');
 const { createDefaultProxy } = require('../../env-utils');
 
-const phpVersions = ['8.3', '8.2', '8.1', '8.0', '7.4', '7.3', '7.2', '7.1', '7.0', '5.6'];
+const phpVersions = [
+	'8.5',
+	'8.4',
+	'8.3',
+	'8.2',
+	'8.1',
+	'8.0',
+	'7.4',
+	'7.3',
+	'7.2',
+	'7.1',
+	'7.0',
+	'5.6',
+];
 
 const wordpressTypes = [
 	{ name: 'Single Site', value: 'single' },
